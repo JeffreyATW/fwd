78198d22460ba7015d412f14de7221da0544a56b
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2015-08-17 21:13:49 -0700

    Adding lecture materials for class 4

---
+++ series8/class4/script.js
@@ -0,0 +1,12 @@
+var addend1 = 5;
+var addend2 = 10;
+
+var sum = addend1 + addend2;
+
+console.log(sum);
+
+/*alert("The sum is " + sum);
+
+var isItFifteen = confirm("Is the sum 15???");
+
+sum = prompt("What is the sum?");*/

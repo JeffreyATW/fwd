65a1a61488c0dd0a48592c20c93cfbd185eb919c
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2015-08-17 21:49:46 -0700

    All done with class 4

--- series8/class4/script.js
+++ series8/class4/script.js
@@ -10,3 +10,17 @@ console.log(sum);
 var isItFifteen = confirm("Is the sum 15???");
 
 sum = prompt("What is the sum?");*/
+
+var age = prompt("How old are you?");
+
+age = parseInt(age);
+
+if (isNaN(age)) {
+  alert("You screwed up!");
+} else if (age < 200) {
+  alert("You're so young!");
+} else if (age === 29) {
+  alert("That's my age!");
+} else {
+  alert("Wow!");
+} 
